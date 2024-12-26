397d1887727b0d74fbe2dbfa5d11f46d05622220
Bogomolova <BatAnn04@yandex.ru>
2024-12-26 13:40:57 +0300

    fix kafka1

--- src/main/java/ru/t1/OpenSchoolT1/kafka/KafkaTaskConsumer.java
+++ src/main/java/ru/t1/OpenSchoolT1/kafka/KafkaTaskConsumer.java
@@ -3,9 +3,12 @@ package ru.t1.OpenSchoolT1.kafka;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 import ru.t1.OpenSchoolT1.dto.TaskDTO;
 import ru.t1.OpenSchoolT1.service.NotificationService;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,10 +18,16 @@ public class KafkaTaskConsumer {
     private final NotificationService notificationService;
 
     @KafkaListener(topics = "task-updates", groupId = "${spring.kafka.consumer.group-id}", containerFactory = "kafkaListenerContainerFactory")
-    public void listenTaskStatusUpdate(TaskDTO taskDTO) {
-        log.info("Received task update for task ID: {}", taskDTO.getId());
-
-        notificationService.sendNotification(taskDTO);
-        log.info("Successfully processed task update and sent notification for task ID: {}", taskDTO.getId());
+    public void listenTaskStatusUpdate(List<TaskDTO> taskDTOs, Acknowledgment acknowledgment) {
+        for (TaskDTO taskDTO : taskDTOs) {
+            try {
+                log.info("Received task update for task ID: {}", taskDTO.getId());
+                notificationService.sendNotification(taskDTO);
+                log.info("Successfully processed task update and sent notification for task ID: {}", taskDTO.getId());
+            } catch (Exception e) {
+                log.error("Error processing task update for task ID: {}", taskDTO.getId(), e);
+            }
+        }
+        acknowledgment.acknowledge();
     }
 }
--- src/main/java/ru/t1/OpenSchoolT1/service/NotificationService.java
+++ src/main/java/ru/t1/OpenSchoolT1/service/NotificationService.java
@@ -2,7 +2,6 @@ package ru.t1.OpenSchoolT1.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,13 +14,10 @@ public class NotificationService {
 
     private final JavaMailSender emailSender;
 
-    @Value("${spring.mail.username}")
-    private String email;
-
     public void sendNotification(TaskDTO taskDTO) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(email);
-        message.setTo(email);
+        message.setFrom("ann9304@rambler.ru");
+        message.setTo("ann1111@rambler.ru");
         message.setSubject("Task Status Update");
         message.setText("Task with ID: " + taskDTO.getUserId() + " has been updated. New status: " + taskDTO.getStatus());
 
